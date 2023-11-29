ba61be1e5c807a8d1e1cf9e03e9a0e1fb544d1c6
Hugo Ramon Pereira <hramonp-dev@outlook.com>
2023-11-28 21:05:49 -0300

    added prettier and fixed errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,20 @@
-'use client';
+"use client";
 
-import Form from '@/components/form';
-import CustomLink from '@/components/form/buttons/link';
-import InputBase from '@/components/form/inputs/inputBase';
-import InputPassword from '@/components/form/inputs/inputPassword';
-import TitleText from '@/components/titleText';
-import useHttp from '@/services/useHttp';
-import LoginIcon from '@mui/icons-material/Login';
-import { useForm } from 'react-hook-form';
-import { z } from 'zod';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { toast } from 'react-hot-toast';
-import { useAuth } from '@/contexts/auth';
-import { LoadingBtn } from '@/components/form/buttons/loadingButton';
-import Spinner from '@/assets/loading-spinner.svg';
-import Image from 'next/image';
+import Form from "@/components/form";
+import CustomLink from "@/components/form/buttons/link";
+import InputBase from "@/components/form/inputs/inputBase";
+import InputPassword from "@/components/form/inputs/inputPassword";
+import TitleText from "@/components/titleText";
+import useHttp from "@/services/useHttp";
+import LoginIcon from "@mui/icons-material/Login";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { toast } from "react-hot-toast";
+import { useAuth } from "@/contexts/auth";
+import { LoadingBtn } from "@/components/form/buttons/loadingButton";
+import Spinner from "@/assets/loading-spinner.svg";
+import Image from "next/image";
 
 interface SignInProps {
   username: string;
@@ -22,65 +22,53 @@ interface SignInProps {
 }
 
 const signInFormSchema = z.object({
-	username: z.string(),
-	password: z.string().min(3).max(20)
+  username: z.string(),
+  password: z.string().min(3).max(20),
 });
 
 export default function SignIn() {
-	const auth = useAuth();
-	const { handleSubmit, control, reset, setError } = useForm<SignInProps>({
-		resolver: zodResolver(signInFormSchema),
-		defaultValues: {
-			username: 'caioluan010',
-			password: 'Caio@123'
-		}
-	});
-	const { Post, isLoading } = useHttp({ url: 'auth/signIn', setError: setError });
-	console.log('setError:', setError);
+  const auth = useAuth();
+  const { handleSubmit, control, reset, setError } = useForm<SignInProps>({
+    resolver: zodResolver(signInFormSchema),
+    defaultValues: {
+      username: "caioluan010",
+      password: "Caio@123",
+    },
+  });
+  const { Post, isLoading } = useHttp({ url: "auth/signIn" });
+  console.log("setError:", setError);
 
-	async function onSubmit(data: SignInProps) {
-		const response = await Post({ body: {...data} });
+  async function onSubmit(data: SignInProps) {
+    const response = await Post({ body: { ...data } });
 
-		if(response.access_token){
-			toast.success('Login successful!');
-			auth.signIn(response);
-			reset();
-			return;
-		}
+    if (response.access_token) {
+      toast.success("Login successful!");
+      auth.signIn(response);
+      reset();
+      return;
+    }
 
-		toast.error('Login failed!');
-	}
+    toast.error("Login failed!");
+  }
 
-	return (
-		<Form
-			onSubmit={handleSubmit(onSubmit)}
-		>
-			<TitleText>Welcome</TitleText>
-			<InputBase
-				label='Username'
-				name='username'
-				control={control}
-			/>
-			<InputPassword
-				label='Password'
-				name='password'
-				control={control}
-			/>
-			<CustomLink
-				href='/signup'
-			>
-          Don&apos;t have an account?
-			</CustomLink>
-			<LoadingBtn
-				variant="contained"
-				type='submit'
-				size='large'
-				loading={isLoading}
-				loadingIndicator={<Image src={Spinner} width={25} height={25} alt='spinner' />}
-				endIcon={<LoginIcon />}
-			>
-				Sign in
-			</LoadingBtn>
-		</Form>
-	);
+  return (
+    <Form onSubmit={handleSubmit(onSubmit)}>
+      <TitleText>Welcome</TitleText>
+      <InputBase label="Username" name="username" control={control} />
+      <InputPassword label="Password" name="password" control={control} />
+      <CustomLink href="/signup">Don&apos;t have an account?</CustomLink>
+      <LoadingBtn
+        variant="contained"
+        type="submit"
+        size="large"
+        loading={isLoading}
+        loadingIndicator={
+          <Image src={Spinner} width={25} height={25} alt="spinner" />
+        }
+        endIcon={<LoginIcon />}
+      >
+        Sign in
+      </LoadingBtn>
+    </Form>
+  );
 }
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -25,101 +25,82 @@ interface SignUpProps {
   confirmPassword: string;
 }
 
-const signUpFormSchema = z.object({
-	fullName: z.string(),
-	username: z.string(),
-	email: z.string().email(),
-	password: z.string().min(3).max(20),
-	confirmPassword: z.string().min(3).max(20),
-}).refine((data) => data.password === data.confirmPassword, {
-	message: 'Passwords don&apos;t match',
-	path: ['confirmPassword']
-});
+const signUpFormSchema = z
+  .object({
+    fullName: z.string(),
+    username: z.string(),
+    email: z.string().email(),
+    password: z.string().min(3).max(20),
+    confirmPassword: z.string().min(3).max(20)
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: 'Passwords don&apos;t match',
+    path: ['confirmPassword']
+  });
 
 export default function SignUp() {
-	const router = useRouter();
-	const { handleSubmit, control, reset, setError } = useForm<SignUpProps>({
-		resolver: zodResolver(signUpFormSchema),
-		defaultValues: {
-			fullName: 'ramon',
-			username: 'ramonp',
-			email: 'ramone.techie@gmail.com',
-			password: '0123456789',
-			confirmPassword: '0123456789'
-		}
-	});
+  const router = useRouter();
+  const { handleSubmit, control, reset, setError } = useForm<SignUpProps>({
+    resolver: zodResolver(signUpFormSchema),
+    defaultValues: {
+      fullName: 'ramon',
+      username: 'ramonp',
+      email: 'ramone.techie@gmail.com',
+      password: '0123456789',
+      confirmPassword: '0123456789'
+    }
+  });
 
-	useEffect(() => {
-		setError('password', {
-			type: 'manual',
-			message: 'Dont Forget Your Username Should Be Cool!',
-		});
-	}, [setError]);
+  useEffect(() => {
+    setError('password', {
+      type: 'manual',
+      message: 'Dont Forget Your Username Should Be Cool!'
+    });
+  }, [setError]);
 
-	const { Post, isLoading } = useHttp({ url: 'auth/signUp', setError: (name, error) => setError(name, error)});
+  const { Post, isLoading } = useHttp({
+    url: 'auth/signUp',
+    setError: (name, error) => setError(name, error)
+  });
 
-	async function onSubmit(data: SignUpProps){
-		const response = await Post({ body: {...data} });
+  async function onSubmit(data: SignUpProps) {
+    const response = await Post({ body: { ...data } });
 
-		if(response) {
-			toast.success('Account created successfully!');
-			reset();
-			router.push('/');
-			return;
-		}
-	}
+    if (response) {
+      toast.success('Account created successfully!');
+      reset();
+      router.push('/');
+      return;
+    }
+  }
 
-	return (
-		<Form
-			onSubmit={handleSubmit(onSubmit)}
-		>
-			<Styled.Title
-				variant="h4"
-			>
-        New account
-			</Styled.Title>
-			<InputBase
-				label='Full Name'
-				name='fullName'
-				control={control}
-			/>
-			<InputBase
-				label='Username'
-				name='username'
-				control={control}
-			/>
-			<InputBase
-				label='Email'
-				name='email'
-				control={control}
-			/>
-			<InputPassword
-				label='Password'
-				name='password'
-				control={control}
-			/>
-			<InputPassword
-				label='Confirm password'
-				name='confirmPassword'
-				control={control}
-			/>
-			<LoadingBtn
-				disableRipple
-				disableElevation
-				variant="contained"
-				size='large'
-				type='submit'
-				loading={isLoading}
-				loadingIndicator={<Image src={Spinner} width={25} height={25} alt='spinner' />}
-				endIcon={<PersonAddAlt1Icon />}
-			>
+  return (
+    <Form onSubmit={handleSubmit(onSubmit)}>
+      <Styled.Title variant='h4'>New account</Styled.Title>
+      <InputBase label='Full Name' name='fullName' control={control} />
+      <InputBase label='Username' name='username' control={control} />
+      <InputBase label='Email' name='email' control={control} />
+      <InputPassword label='Password' name='password' control={control} />
+      <InputPassword
+        label='Confirm password'
+        name='confirmPassword'
+        control={control}
+      />
+      <LoadingBtn
+        disableRipple
+        disableElevation
+        variant='contained'
+        size='large'
+        type='submit'
+        loading={isLoading}
+        loadingIndicator={
+          <Image src={Spinner} width={25} height={25} alt='spinner' />
+        }
+        endIcon={<PersonAddAlt1Icon />}
+      >
         Sign up
-			</LoadingBtn>
-			<CustomLink
-				href='/'
-			>
-        Back home
-			</CustomLink>
-		</Form>
-	);
+      </LoadingBtn>
+      <CustomLink href='/'>Back home</CustomLink>
+    </Form>
+  );
 }
